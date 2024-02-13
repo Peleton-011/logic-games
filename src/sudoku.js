8d3c13d3ea44a49526fe9cae08499b01232221e5
Nico Diaz <nicodiazan@gmail.com>
2024-02-13 15:58:04 +0100

    Add some boilerplate for board generation

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,6 +1,49 @@
 class sudoku {
-	constructor(size) {
-        this.size = size || 3;
+	unitBoard = [
+		[1, 2, 3, 4, 5, 6, 7, 8, 9],
+		[4, 5, 6, 7, 8, 9, 1, 2, 3],
+		[7, 8, 9, 1, 2, 3, 4, 5, 6],
+
+		[2, 3, 1, 5, 6, 4, 8, 9, 7],
+		[5, 6, 4, 8, 9, 7, 2, 3, 1],
+		[8, 9, 7, 2, 3, 1, 5, 6, 4],
+
+		[3, 1, 2, 6, 4, 5, 9, 7, 8],
+		[6, 4, 5, 9, 7, 8, 3, 1, 2],
+		[9, 7, 8, 3, 1, 2, 6, 4, 5],
+	];
+
+	board = [];
+	#newBoardByShuffle() {
+        //Deep copy the unitBoard
+		this.board = this.unitBoard.map((elem) => elem.map((elem) => elem));
     }
+
+	#shuffleNumbers() {
+		for (i = 0; i < 9; i++) {
+			const ranNum = this.#getRandomInt(this.size ** 2) + 1;
+			this.#swapNumbers(i, ranNum);
+		}
+	}
+
+	#swapNumbers(a, b) {
+		for (y = 0; y < 9; y++) {
+			for (x = 0; x < 9; x++) {
+				if (this.board[x][y] == a) {
+					this.board[x][y] = b;
+				} else if (this.board[x][y] == b) {
+					this.board[x][y] = a;
+				}
+			}
+		}
+	}
+
+	#getRandomInt(max) {
+		return Math.floor(Math.random() * max);
+	}
+
+	constructor(size) {
+		this.size = size || 3;
+	}
 }
-export default sudoku
+export default sudoku;
