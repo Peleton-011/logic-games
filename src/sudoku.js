54fcf631ae963cd25b2e639160c8719de1e222a6
Nico Diaz <nicodiazan@gmail.com>
2024-02-20 08:07:05 +0100

    Begin implementing some board generation methods

--- src/sudoku.js
+++ src/sudoku.js
@@ -13,22 +13,54 @@ class sudoku {
 		[9, 7, 8, 3, 1, 2, 6, 4, 5],
 	];
 
-	board = [];
+	constructor(size) {
+		this.size = size || 3;
+		this.fullSize = this.size ** 2;
+		this.board = [];
+		this.#newBoardByShuffle();
+		console.log(this.board);
+	}
 	#newBoardByShuffle() {
-        //Deep copy the unitBoard
-		this.board = this.unitBoard.map((elem) => elem.map((elem) => elem));
-    }
+		//Deep copy the unitBoard
+		console.log("Before deepCopy :");
+		console.log(this.board);
+		//Make an ordered board
+		const orderedBoard = new Array(9)
+			.fill(0)
+			.map((val, index) =>
+				new Array(9)
+					.fill(0)
+					.map((val, subindex) => index * 9 + subindex)
+			);
+		//console.log(orderedBoard);
+		this.board = this.unitBoard.map((elem) => {
+			console.log("row: ");
+			console.log(elem);
+			return elem.map((cell) => {
+				//console.log("cell");
+				//console.log(cell);
+				return cell;
+			});
+		});
+		console.log("After deepCopy :");
+		console.log(this.board);
+		//swap numbers
+		//this.#shuffleNumbers();
+		//shuffle rows and columns, then subgrid rows and columns
+		//this.#shuffleRows();
+		//this.#shuffleColumns();
+	}
 
 	#shuffleNumbers() {
-		for (i = 0; i < 9; i++) {
-			const ranNum = this.#getRandomInt(this.size ** 2) + 1;
+		for (let i = 0; i < this.fullSize; i++) {
+			const ranNum = this.#getRandomInt(this.fullSize) + 1;
 			this.#swapNumbers(i, ranNum);
 		}
 	}
 
 	#swapNumbers(a, b) {
-		for (y = 0; y < 9; y++) {
-			for (x = 0; x < 9; x++) {
+		for (let y = 0; y < this.fullSize; y++) {
+			for (let x = 0; x < this.fullSize; x++) {
 				if (this.board[x][y] == a) {
 					this.board[x][y] = b;
 				} else if (this.board[x][y] == b) {
@@ -38,12 +70,42 @@ class sudoku {
 		}
 	}
 
-	#getRandomInt(max) {
-		return Math.floor(Math.random() * max);
+	#shuffleRows() {
+		let blockNumber;
+
+		for (let i = 0; i < this.fullSize; i++) {
+			const ranNum = this.#getRandomInt(this.size) + 1;
+			blockNumber = i / this.size;
+			this.#swapRows(i, blockNumber * this.size + ranNum);
+		}
 	}
 
-	constructor(size) {
-		this.size = size || 3;
+	#swapRows(r1, r2) {
+		const row = this.board[r1];
+		this.board[r1] = this.board[r2];
+		this.board[r2] = row;
+	}
+
+	#shuffleColumns() {
+		let blockNumber;
+
+		for (let i = 0; i < this.fullSize; i++) {
+			const ranNum = this.#getRandomInt(this.size) + 1;
+			blockNumber = i / this.size;
+			this.#swapColumns(i, blockNumber * this.size + ranNum);
+		}
+	}
+	#swapColumns(c1, c2) {
+		let colVal;
+		for (let i = 0; i < 9; i++) {
+			colVal = this.board[i][c1];
+			this.board[i][c1] = this.board[i][c2];
+			this.board[i][c2] = colVal;
+		}
+	}
+
+	#getRandomInt(max) {
+		return Math.floor(Math.random() * max);
 	}
 }
 export default sudoku;
